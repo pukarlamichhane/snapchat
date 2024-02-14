1998a199853d63a189a3f499a1d830819bd682bc
pukarlamichhane <pukarlamichhane567@gmail.com>
2024-02-14 21:07:10 +0545

    complete app

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+import LoginCard from "./login-card";
+
+export default async function Login() {
+	return (
+		<>
+			<h1 className='text-2xl font-bold text-center mb-4'>Log in to SnapNext</h1>
+			<LoginCard />
+		</>
+	);
+}
+
---
+++ components/chat/user-card.tsx
@@ -0,0 +1,26 @@
+import { IUserDocument } from "@/models/userModel";
+import { Avatar, AvatarImage } from "../ui/avatar";
+
+type UserCardProps = {
+	user: IUserDocument;
+	handleSelectUser: (user: IUserDocument) => void;
+	selectedUser: IUserDocument | null;
+};
+const UserCard = ({ user, handleSelectUser, selectedUser }: UserCardProps) => {
+	const isSelected = selectedUser?._id === user._id;
+	return (
+		<div
+			onClick={() => handleSelectUser(user)}
+			className={`flex items-center gap-2 border-b border-b-sigColorBgBorder p-1 hover:bg-sigBackgroundFeedHover cursor-pointer 
+				${isSelected ? "bg-sigBackgroundFeedHover" : ""}
+			`}
+		>
+			<Avatar className='cursor-pointer hover:bg-sigBackgroundSecondaryHover'>
+				<AvatarImage src={user.avatar} />
+			</Avatar>
+			<span>{user.fullName}</span>
+		</div>
+	);
+};
+export default UserCard;
+
